8c6173a304641808ec9333bfa27bf3882a648466
Nicodev <nicolascarobene3@gmail.com>
2024-02-18 13:09:26 +0100

    add endpoint contact and fix formContact

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,15 +1,16 @@
 import type { APIRoute } from 'astro'
 export const POST: APIRoute = async ({ request }) => {
-  const formData = await request.formData()
-  const email = formData.get('email')
-  const message = formData.get('message')
-  const name = formData.get('name')
+  const { email, message, name } = await request.json()
   if (!email || !message || !name) {
     return new Response('Bad request', {
       status: 400,
     })
   }
-  return new Response('OK', {
+  return new Response(JSON.stringify({
+    name,
+    email,
+    message
+  }), {
     status: 200,
   })
 }
