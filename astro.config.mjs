3d99559d97d6bfe98d3f825799b996d31dbf9127
nicodev <nicolascarobene1>
2024-03-30 11:55:16 +0100

    change import vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
     }
   },
   output: 'hybrid',
-  adapter: vercel({
+  adapter: vercelServerless({
     webAnalytics: {
       enabled: true
     }
